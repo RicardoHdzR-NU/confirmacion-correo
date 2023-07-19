6dfa06ce51fab0f26e1bbd24da77932dd1a08c50
RicardoHdzR-NU <139916385+RicardoHdzR-NU@users.noreply.github.com>
2023-07-19 10:50:36 -0600

    template inicial

--- src/components/EmailCard.jsx
+++ src/components/EmailCard.jsx
@@ -5,6 +5,7 @@ import NU_logo from '../img/nu-logo.png'
 import Facebook from '../img/facebook.svg'
 import Instagram from '../img/instagram.svg'
 import Mail from '../img/envelope-fill.svg'
+import Wha from '../img/whatsapp.svg'
 
 import './EmailCard.css'
 
@@ -12,44 +13,54 @@ function EmailCard(){
     return(
         <Container >
             <Row className='mb-4 justify-content-center'>
-                <Image src={NU_logo} style={{width: '18rem'}}></Image>
+                <Image src={NU_logo} style={{width: '12rem'}}></Image>
             </Row>
-            <Card className='cuerpo p-0' style={{width: '25rem'}}>
+            <Card className='cuerpo p-0' style={{width: '28rem'}}>
                 <Card.Body className='text-start'>
-                    <Card.Title className='titulo justify-content-center'>Confirmación de Transacción</Card.Title>
-                    <Card.Text className='contenido'>
-                    National Unity Insurance Company<br/>
-                    One Huebner Parke 15303, Huebner Road<br/>
-                    San Antonio, Tx. 78248<br/>
-                    "2023-07-18 09:00:13"<br/>
-                    SALE<br/>
-                    ACCOUNT :
-                    **** **** **** "1562"<br/>
-                    APPROVAL : "0JX5R1"<br/>
-                    TRANS ID : "40123566455"<br/>
-                    REFERENCE :  "ARR-343"<br/>
-                    TOTAL PAID $ : "1" USD<br/>
-                    </Card.Text>
+                    <Card.Title className='titulo text-center'>¡Pago exitoso!</Card.Title>
+                    <Card.Text className='fecha text-center'>2023-07-18 09:00:13</Card.Text>
+                    <hr/>
+                    <Row className='justify-content-center pago'>
+                        <Col className='text-start'>Monto Total :</Col>
+                        <Col className='text-end'>1 USD</Col>
+                    </Row>
+                    <Row className='justify-content-center contenido'>
+                        <Col className='text-start'>Póliza :</Col>
+                        <Col className='text-end'>ARR-343</Col>
+                    </Row>
+                    <Row className='justify-content-center contenido'>
+                        <Col className='text-start'>ID de transacción :</Col>
+                        <Col className='text-end'>40123566455</Col>
+                    </Row>
+                    <Row className='justify-content-center contenido'>
+                        <Col className='text-start'>Método de pago :</Col>
+                        <Col className='text-end'>**** **** **** "1562"</Col>
+                    </Row>
                 </Card.Body>
             </Card>
-            <Row className='mt-4 justify-content-center' style={{width: '25rem'}}>
-                <Col xs lg="5">
+            <Row className='mt-4 justify-content-center' style={{width: '28rem'}}>
+                <Col xs lg="3">
                     <a href='https://www.facebook.com/NationalUnityIC/'>
                         <Image width={32} height={32} src={Facebook}/>
                     </a>
                 </Col>
                 
-                <Col xs lg="5">
+                <Col xs lg="3">
                     <a href='https://www.instagram.com/nationalunity_seguros/'>
                         <Image width={32} height={32} src={Instagram}/>
                     </a>
                 </Col>
+                <Col xs lg="3">
+                    <a href='https://www.instagram.com/nationalunity_seguros/'>
+                        <Image width={32} height={32} src={Wha}/>
+                    </a>
+                </Col>
             </Row>
-            <Row className='justify-content-center mt-4' style={{width: '25rem'}}>
-                 <Col md="auto">
+            <Row className='mt-4 justify-content-center' style={{width: '28rem'}}>
+                 <Col xs='auto' className='p-1'>
                     <Image width={32} height={32} src={Mail}/> 
                 </Col>
-                <Col md="auto" className='text-start contenido mt-1'>servicoalcliente@nationalunity.com.mx</Col>
+                <Col xs="auto" className='mail p-1 mt-2'>servicoalcliente@nationalunity.com.mx</Col>
             </Row>
         </Container>
         
